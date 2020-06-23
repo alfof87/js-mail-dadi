89efc3790e5000055f8d0648936762cd55b7d03a
Alfonso <alfof87@gmail.com>
2020-06-23 15:08:42 +0200

    prova 1

---
+++ dadi/js/script.js
@@ -0,0 +1,10 @@
+// var dado1 = Math.floor(Math.random()*6);
+// var dado2 = Math.floor(Math.random()*6);
+// var num =[1, 2, 3, 4, 5, 6];
+//
+// for (i=0; i > 5; i++){
+//   console.log()
+// }
+
+var dado1 = Math.floor(Math.random()*6);
+var dado2 = Math.floor(Math.random()*6);
