3394170ab5cd684e4ade26c31f7744b5ff612afb
Alfonso <alfof87@gmail.com>
2020-06-23 18:45:20 +0200

    prova mail II

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -9,16 +9,16 @@
 document.getElementById('btn');
 
 document.getElementById('btn').addEventListener("click", function(){
-  
+
   var dado1 = Math.floor(Math.random()*6 + 1);
   var dado2 = Math.floor(Math.random()*6 + 1);
   var vittoria;
   var parita = dado1 = dado2;
   if (dado1 > dado2) {
-    console.log(vittoria = dado1)
+    console.log(vittoria = dado1);
   }else if(dado2 > dado1){
-    console.log(vittoria = dado2)
+    console.log(vittoria = dado2);
   }else {
-    console.log(vittoria = parita)
+    console.log(vittoria = parita);
   }
 });
--- mail/js/script.js
+++ mail/js/script.js
@@ -1,6 +1,12 @@
 //////////////////////////////////////////////////MAIL/////////
 
 var mail = prompt("inserisci mail");
-var lista = ["marcomail", "lucamail", "giovannimail", "stefanomail"];
+var lista = ["marco@gmail.com", "luca@gmail.com", "giovanni@gmail.com", "stefano@gmail.com"];
 var i;
-for( i = 0; i > 4; i++)
+for( i = 0; i < lista.length; i++){
+  if (mail == lista[i]){
+    console.log("mail presente");
+  }else {
+    console.log("mail non presente");
+  }
+};
