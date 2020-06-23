92f3c5158c34fd987f196131f9e5f543f77b328d
Alfonso <alfof87@gmail.com>
2020-06-23 20:23:02 +0200

    prova 3

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,24 +1,50 @@
 ///////////////////////////////////////////////////////DADI
+// document.getElementById('btn');
 // var dado1 = Math.floor(Math.random()*6);
 // var dado2 = Math.floor(Math.random()*6);
 // var num =[1, 2, 3, 4, 5, 6];
 //
 // for (i=0; i > 5; i++){
-//   console.log()
-// }
-document.getElementById('btn');
+//   if(dado1 > dado2){
+//     console.log("dado1 ha vinto!");
+//   }else if (dado1 < dado2){
+//     console.log("dado2 ha vinto!");
+//   }else{
+//     console.log("parità!");
+//   }
+// };
 
-document.getElementById('btn').addEventListener("click", function(){
 
-  var dado1 = Math.floor(Math.random()*6 + 1);
-  var dado2 = Math.floor(Math.random()*6 + 1);
-  var vittoria;
-  var parita = dado1 = dado2;
+
+
+// document.getElementById('btn');
+//
+// document.getElementById('btn').addEventListener("click", function(){
+//
+//   var dado1 = Math.floor(Math.random()*6 + 1);
+//   var dado2 = Math.floor(Math.random()*6 + 1);
+//   var vittoria;
+//   var parita = dado1 = dado2;
+//   if (dado1 > dado2) {
+//     console.log(vittoria = dado1);
+//   }else if(dado2 > dado1){
+//     console.log(vittoria = dado2);
+//   }else {
+//     console.log(vittoria = parita);
+//   }
+// });
+
+
+
+ var dado1 = Math.floor(Math.random()*6 + 1);
+ var dado2 = Math.floor(Math.random()*6 + 1);
+ var vittoria;
+ var parita = dado1 = dado2;
+ 
   if (dado1 > dado2) {
-    console.log(vittoria = dado1);
+    console.log("Vince dado 1");
   }else if(dado2 > dado1){
-    console.log(vittoria = dado2);
+    console.log("Vince dado2");
   }else {
-    console.log(vittoria = parita);
-  }
-});
+    console.log("Pareggio");
+  };
