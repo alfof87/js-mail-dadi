f34e760d93c003bf7efba5db5900811d0046c1e7
Alfonso <alfof87@gmail.com>
2020-06-23 15:20:29 +0200

    prova dadi con if

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -5,6 +5,18 @@
 // for (i=0; i > 5; i++){
 //   console.log()
 // }
+document.getElementById('btn');
 
-var dado1 = Math.floor(Math.random()*6);
-var dado2 = Math.floor(Math.random()*6);
+document.getElementById('btn').addEventListener("click", function(){
+  var dado1 = Math.floor(Math.random()*6 + 1);
+  var dado2 = Math.floor(Math.random()*6 + 1);
+  var vittoria;
+  var parita = dado1 = dado2;
+  if (dado1 > dado2) {
+    console.log(vittoria = dado1)
+  }else if(dado2 > dado1){
+    console.log(vittoria = dado2)
+  }else {
+    console.log(vittoria = parita)
+  }
+});
